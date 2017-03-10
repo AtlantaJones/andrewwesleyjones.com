81182fc79a8e8a4e41fe161f0b19a384d11f6c99
Andrew Jones <andrew@atlantajones.com>
2017-03-10 15:32:39 -0500

    Responsive work at phone sizes; added mobile menu toggle

--- js/app.js
+++ js/app.js
@@ -468,7 +468,24 @@ myApp.animation('.page-container', [function() {
 
 myApp.controller('mainController', ['$scope', '$location', function ($scope, $location) {
 
-        console.log('I am main');
+    $scope.menuOpen = false;
+
+    $scope.toggleMenu = function() {
+        var transition = ($scope.menuOpen) ? 'transition.slideLeftOut' : 'transition.slideLeftIn';
+        $('nav').velocity(transition, {
+            duration: 200
+        });
+        $scope.menuOpen = !$scope.menuOpen;
+    };
+
+    $scope.closeMenu = function() {
+        if($scope.menuOpen) {
+            $('nav').velocity('transition.slideLeftOut', {
+                duration: 200
+            });
+            $scope.menuOpen = false;
+        }
+    }
 
 
 }]);
